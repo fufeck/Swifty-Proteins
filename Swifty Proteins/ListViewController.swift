8b8d2540cd26ea0fa0c66a458ebd6cb25848c6e0
Fabien TAFFOREAU <ftaffore@e1r8p15.42.fr>
2016-10-20 15:36:10 +0200

    first commit

---
+++ Swifty Proteins/ListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewController.swift
+//  Swifty Proteins
+//
+//  Created by Fabien TAFFOREAU on 10/20/16.
+//  Copyright © 2016 Fabien TAFFOREAU. All rights reserved.
+//
+
+import Foundation
---
+++ Swifty Proteins/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Swifty Proteins
+//
+//  Created by Fabien TAFFOREAU on 10/20/16.
+//  Copyright © 2016 Fabien TAFFOREAU. All rights reserved.
+//
+
+import Foundation
